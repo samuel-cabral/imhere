ca1551978d846e7e0ac8100b69d312e182c79357
Samuel Cabral <samuelcabral.mail@gmail.com>
2024-02-14 12:50:31 -0300

    feat: create form

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import React from 'react';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { styles } from './styles';
 
 export function Home() {
+  function handleParticipantAdd() {
+    console.log('Você adicionou um participante!');
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
       <Text style={styles.eventDate}>Quarta, 14 de fevereiro</Text>
+
+      <View style={styles.form}>
+        <TextInput style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
       
       <StatusBar style="auto" />
     </View>
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -16,4 +16,36 @@ export const styles = StyleSheet.create({
     color: '#6B6B6B',
     fontSize: 16,
   },
+  input: {
+    flex: 1,
+    backgroundColor: '#1F1E25',
+    borderRadius: 5,
+    color: '#FDFCFE',
+    height: 56,
+    marginTop: 24,
+    padding: 16,
+    fontSize: 16,
+    marginRight: 12,
+  },
+  buttonText: {
+    color: '#FDFCFE',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  button: {
+    backgroundColor: '#31CF67',
+    borderRadius: 5,
+    height: 56,
+    width: 56,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 24,
+  },
+  form: {
+    marginTop: 36,
+    marginBottom: 42,
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+  }
 });
